310e0032b33865186723d80ffd457ff43c77580a
Oleg Pudeyev <code@olegp.name>
2025-11-01 21:38:04 -0400

    improving test listing

--- support/test-info.py
+++ support/test-info.py
@@ -10,22 +10,23 @@ from utils import context
 class FakeScenario:
     def __init__(self):
         self.library = Library('ruby')
+        self.name = 'Fake'
 context.scenario = FakeScenario()
 
 info = {}
 
-mod_name = sys.argv[1]
-__import__(mod_name)
-mod = sys.modules[mod_name]
-info[mod_name] = {}
-for cls_name in dir(mod):
-    if re.match('Test_', cls_name):
-        cls = getattr(mod, cls_name)
-        if hasattr(cls, 'pytestmark'):
-            for mark in cls.pytestmark:
-                if mark.name == 'scenario':
-                    info[mod_name][cls_name] = {
-                        'scenario': mark.args[0],
-                    }
+for mod_name in sys.argv[1:]:
+    __import__(mod_name)
+    mod = sys.modules[mod_name]
+    info[mod_name] = {}
+    for cls_name in dir(mod):
+        if re.match('Test_', cls_name):
+            cls = getattr(mod, cls_name)
+            if hasattr(cls, 'pytestmark'):
+                for mark in cls.pytestmark:
+                    if mark.name == 'scenario':
+                        info[mod_name][cls_name] = {
+                            'scenario': mark.args[0],
+                        }
 
 print(json.dumps(info))
